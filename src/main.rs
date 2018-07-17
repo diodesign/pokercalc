c3cb88adeaeb7c27cd91d78bd7b33fbdead83e71
Chris Williams <diodesign@gmail.com>
2018-07-16 21:04:39 -0700

    First commit to create project

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+/* pokercalc
+ * Calculate the cards required to beat you in a given hand of Texas Hold 'em
+ *
+ * (c) Chris Williams, 2018. Open-source software: see LICENSE
+ */
+
+fn main()
+{
+  println!("Hello, world!");
+}
